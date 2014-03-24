9617373840af77470c2ccd57d9ab24d13e08b638
Noufal Ibrahim <noufal@ideadevice.com>
2014-03-24 14:30:55 +0530

    Basic commands working

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-import gevent
-
-from gevent.pool import Pool
-
-pool = Pool(2)
+from gevent.server import StreamServer
 
 class Pretty(object):
     def __repr__(self):
         return "<{} : {}>".format(self.__class__.__name__, self.name)
 
 class Person(Pretty):
-    def __init__(self, name, room = None):
+    def __init__(self, name, room = None, items = None):
         self.name = name
         self.room = room
+        self.items = items or []
+
+    def inventory(self):
+        return "You are carrying \n" + "\n".join(" %s"%x.name for x in self.items) + "\n"
+        
 
 class Item(Pretty):
     def __init__(self, name, description):
@@ -26,20 +27,95 @@ class Room(Pretty):
         self.people = people or []
         self.north = self.south = self.east = self.west = None
 
-class Mansion(Pretty):
+
+class Mansion():
     def __init__(self, rooms):
         self.rooms = rooms
         self.people = {}
-        for i in self.rooms:
-            for p in i.people:
-                self.people[p] = i
+        for r in self.rooms:
+            for p in r.people:
+                self.people[p] = r
+
+    def place(self, person, room):
+        print "Placing %s in %s"%(person, room)
+        self.people[person] = room
+        person.room = room
+        room.people.append(person)
+    
+    def remove(self, person):
+        print "%s is leaving"%person
+        person.room.people.remove(person)
+        del self.people[person]
+
+    def move(self, person, room):
+        if not room:
+            r = "I'm afraid you can't go that way"
+        else:
+            r = "You move into the %s"%room.name
+            self.people[person] = room
+            person.room.people.remove(person)
+            person.room = room
+            person.room.people.append(person)
+        return r
+
+    
+    def room_info(self, person):
+        info =  """You are currently in the {}.
+        
+{}""".format(person.room.name, person.room.description)
+
+        things = ""
+        if person.room.items:
+            things = "I can see the following things here - {}".format(", ".join(x.name for x in person.room.items))
+        
+        people = ""
+        if [x for x in person.room.people if x is not person]:
+            people = "The following people are in the room - {}".format(", ".join(x.name for x in person.room.people if x is not person))
+        
+        return "\n".join([info, things, people])
+
+    def pick_up(self, person, item_name):
+        item = [x for x in person.room.items if x.name == item_name]
+        item = item and item[0]
+        if item:
+            person.room.items.remove(item)
+            person.items.append(item)
+            return "You pick up the %s\n"%item.name
+        else:
+            return "I see no %s here"%item_name
+            
+    
+    def handle(self, socket, remote):
+        print "Received connection on %s"%socket
+        socket.send("What is your name?\n")
+        name = socket.recv(2048).strip()
+        p = Person(name = name)
+        print "Created %s"%p
+        self.place(p, self.rooms[0])
+        socket.send(self.room_info(p))
+        while True:
+            socket.send("\nWhat do you want to do now?\n")
+            command = socket.recv(2048).strip()
+            if command == "quit":
+                socket.send("Goodbye\n")
+                self.remove(p)
+                return
+            if command == "look":
+                socket.send(self.room_info(p))
+            if command in ["north", "south", "east", "west"]:
+                socket.send(self.move(p, getattr(p.room, command)))
+            if command == "inventory":
+                socket.send(p.inventory())
+            if command.startswith("get"):
+                verb, item = command.split()
+                socket.send(self.pick_up(p, item))
         
 def main():
     # Items
     knife = Item("Knife", "A rusty and blunt knife")
     ladle = Item("Ladle", "A wooden ladle")
     carafe = Item("Carafe", "A crystal carafe. Surprisingly undamaged and clean")
-    sword = Item("Ornamental sword", "A very well made but ancient sword")
+    sword = Item("Sword", "A very well made but ancient sword")
     # Rooms
     garden = Room("Garden", "This is an old an unkempt garden. Weeds peek out from crumbling statues and dry fountains")
     hallway = Room("Hallway", "A high roofed hallway now empty and foreboding", items = [sword])
@@ -54,7 +130,9 @@ def main():
     kitchen.west = dining
     
     m = Mansion([garden, hallway, dining, kitchen])
-    
+    server = StreamServer(('127.0.0.1', 5000), m.handle)
+    print "Server ready"
+    server.serve_forever()
 
 
 if __name__ == '__main__':
